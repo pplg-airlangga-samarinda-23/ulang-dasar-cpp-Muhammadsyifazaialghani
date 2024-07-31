41a1a821aeac4ccb2072973a9e4c5155fe0431d1
Muhammadsyifazaialghani <23_muhammadsyifazai@student.smkti.net>
2024-07-31 11:32:16 +0800

    selesai tugas

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void cetakData(string nama, string nisn, string jurusan, string tinggiBadan) {
+
+cout << "nama :" << nama << endl;
+cout << "nisn :" << nisn << endl;
+cout << "jurusan :" << jurusan << endl;
+cout << "tinggiBadan" << tinggiBadan << endl;
+}
+
 int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
-    return 0;
+    string nama = "Muhammd syifa zai al ghani";
+    string nisn = "0085646403";
+    string jurusan = "X PPLG";
+    string tinggiBadan = "167 cm"; 
+
+cetakData(nama, nisn, jurusan, tinggiBadan);
+
+
+return 0;
 }
